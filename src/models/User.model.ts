9ddf7b4d490527be4fa0f897b81abdb4c65c68b0
rahneverd <rahneverd@gmail.com>
2024-06-16 15:33:22 +0500

    structure updated

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,6 +1,6 @@
 
 import UserInterface from "@/interfaces/user.interface";
-import UserSchema from "@/schemas/user.schema";
+import UserSchema from "@/schemas/db/user.schema";
 import mongoose from "mongoose";
 
 const UserModel = (mongoose.models.User as mongoose.Model<UserInterface>) || mongoose.model<UserInterface>("User", UserSchema)
